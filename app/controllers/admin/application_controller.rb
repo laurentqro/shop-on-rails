01fa7c1ac04262396362e72170a11be65f65595b
Laurent Curau <github@lqro.slmail.me>
2025-05-15 17:36:50 +0200

    Add user roles

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,3 +1,10 @@
 class Admin::ApplicationController < ApplicationController
   layout "admin"
+  before_action :require_admin
+
+  private
+
+  def require_admin
+    redirect_to root_path, alert: "You are not authorized to access this page." unless Current.user.admin?
+  end
 end
