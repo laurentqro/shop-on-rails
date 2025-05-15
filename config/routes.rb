adab73a13b847b8990be6d1c3f69f6b46f44ea83
Laurent Curau <github@lqro.slmail.me>
2025-05-15 23:04:23 +0200

    Update signin/up path names

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root "products#index"
 
   resources :products, only: [ :index, :show ], path: "product"
-  resource :session
+  resource :session, path: "signin", path_names: { new: "" }
   resources :passwords, param: :token
-  resource :registration, only: [ :new, :create ], path: "register", path_names: { new: "" }
+  resource :registration, only: [ :new, :create ], path: "signup", path_names: { new: "" }
 
   namespace :admin do
     get "/", to: "products#index"
