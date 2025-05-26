ece2bfc1acab891e0c0a917866e26b04d69c7495
Laurent Curau <github@lqro.slmail.me>
2025-05-27 00:33:16 +0200

    Add VAT to cart show page

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,12 +3,18 @@ class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :products, through: :cart_items
 
+  VAT_RATE = 0.2
+
   def items_count
     cart_items.sum { |item| item.quantity }
   end
 
   def subtotal_amount
     cart_items.sum { |item| item.subtotal_amount }
+  end
+
+  def vat_amount
+    subtotal_amount * Cart::VAT_RATE
   end
 
   def total_amount
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -8,14 +8,6 @@ class CartItem < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates_uniqueness_of :product, scope: :cart
 
-  def total_amount
-    subtotal_amount + vat_amount
-  end
-
-  def vat_amount
-    subtotal_amount * VAT_RATE
-  end
-
   def subtotal_amount
     price * quantity
   end
