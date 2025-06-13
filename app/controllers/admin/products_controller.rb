53adec7c799f79a62019faf11f9774fcf70e6f76
Laurent Curau <github@lqro.slmail.me>
2025-06-13 12:46:45 +0200

    Fix linting

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -52,7 +52,7 @@ module Admin
     end
 
     private
-      # Use callbacks to share common setup or constraints between actions.
+    # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find_by!(slug: params.expect(:id))
     end
