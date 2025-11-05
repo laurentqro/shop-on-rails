df9946b52ab87318215c33b0f4d9462f3c73221a
Laurent Curau <github@lqro.slmail.me>
2025-11-05 18:05:44 +0100

    Add category icons throughout the application
    - Add category_icon_path helper to map categories to SVG icons
    - Display icons in category navigation with centered layout
    - Show icons on category show pages (in h1 heading)
    - Display large icons in category cards on index and shop pages
    - Add SVG graphics for all categories

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def category_icon_path(category)
+    icon_mapping = {
+      "cups-and-lids" => "images/graphics/cold-cups.svg",
+      "ice-cream-cups" => "images/graphics/ice-cream-cups.svg",
+      "napkins" => "images/graphics/napkins.svg",
+      "pizza-boxes" => "images/graphics/pizza-boxes.svg",
+      "straws" => "images/graphics/straws.svg",
+      "takeaway-containers" => "images/graphics/kraft-food-containers.svg",
+      "takeaway-extras" => "images/graphics/take-away-extras.svg"
+    }
+
+    icon_mapping[category.slug]
+  end
 end
